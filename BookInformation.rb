73caab06a08e5391cfa17c1dc9506be06b7ca3c3
takameron <takameron.info@gmail.com>
2016-09-08 17:47:31 +0900

    データベースの修正,登録時のデータベース操 作など

--- BookInformation.rb
+++ BookInformation.rb
@@ -24,6 +24,26 @@ get '/registration' do
 	erb :registration ,layout: :layout
 end
 
+#登録作業
+get '/registration/insert' do
+	@isbn = params["isbn"]
+	@bookTitle = params["title"]
+	@bookAuthor = params["author"]
+	@bookPublisher = params["publisher"]
+	@publication_year = params["publication_year"]
+	@publication_month = params["publication_month"]
+	@publication_date = params["publication_date"]
+
+	if(@isbn==NULL||@title==NULL){
+		redirect '/registration'
+	}
+
+	sql = <<-SQL
+	  INSERT INTO BookInformation(isbn,title,author,publisher,publication_year,publication_month,publication_date)
+	  VALUES("#{@isbn}","#{@title}","#{@author}","#{@publisher}","#{@publication_year}","#{@publication_month}","#{@publication_date}");
+	SQL
+end
+
 #アプリケーション紹介ページ
 get '/about' do
 	erb :about ,layout: :layout
